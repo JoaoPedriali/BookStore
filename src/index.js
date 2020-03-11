ce385101000d5b712d9dccd91836de4700924730
JPedriali <joao.pedriali@outlook.com>
2020-03-11 11:05:29 -0300

    Added dotEnv dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const router = require('./routes')
 const bodyparser = require('body-parser')
 const app = express()
 
+dotenv.config()
 
 var port = process.env.PORT || 8080
 
@@ -14,9 +16,11 @@ app.use(bodyparser.urlencoded({
 
 app.use(bodyparser.json())
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+var uri = process.env.MONGO_URI
 
-
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
 
 app.get('/',(req, res) => {
     res.send('Hello world')
@@ -25,5 +29,3 @@ app.get('/',(req, res) => {
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
- 
-console.log(process.env.MONGO_URL)
