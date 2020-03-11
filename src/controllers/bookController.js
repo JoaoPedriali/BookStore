f17f5caada26cf765e132055bb1d9dcacaa70bfc
JoaoPedriali <joao.pedriali@outlook.com>
2020-03-10 21:58:20 -0300

    Added the Controller for the book.

---
+++ src/controllers/bookController.js
@@ -0,0 +1,87 @@
+const Book = require('../models/bookModel')
+
+//gets the list of books
+exports.index = (req,res) => {
+    Book.get((err, books) => {
+        if (err){
+            res.json({
+                status:"error",
+                message:err
+            })
+        }
+
+        res.json({
+            status: "sucess",
+            message: "Books retrieved sucessfully",
+            data: books
+        })
+    })
+}
+
+//handles the creation of a book
+exports.new = (req, res) => {
+    var book = new Book()
+    book.title = req.body.title
+    book.author = req.body.author
+    book.synopsis = req.body.synopsis
+    book.price = req.body.price
+    book.inStock = req.body.inStock
+
+    //saves the book
+    book.save((err) => {
+        if (err)
+            res.send(err)
+
+        res.json({
+            message: "New Book Added",
+            data: book
+        })
+    })
+}
+
+//Handles the book information view
+exports.view = (req, res) => {
+    Book.findById(req.params.book_id, (err, book) =>{
+        if(err)
+            res.send(err)
+
+        res.json({
+            message: "Book retrieved",
+            data: book
+        })
+    })
+}
+
+//updates the data in a book
+exports.update = (req, res) => {
+    Book.findById(req.params.book_id, (err, book) => {
+        book.title = req.body.title
+        book.author = req.body.author
+        book.synopsis = req.body.synopsis
+        book.price = req.body.price
+        book.inStock = req.body.inStock
+
+        book.save((err) => {
+            if(err)
+                res.json(err)
+            
+            res.json({
+                message: "Book info updated",
+                data: book
+            })
+        })
+    })
+}
+
+//handles the deletion of a book
+exports.delete = (req, res) => {
+    Book.remove({_id: req.params.book_id}, (err, book) => {
+        if(err)
+            res.send(err)
+
+        res.json({
+            status: "Sucess",
+            message: "Book Deleted"
+        })
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const router = require('./router')
+const router = require('./routes')
 const bodyparser = require('body-parser')
 const app = express()
 
